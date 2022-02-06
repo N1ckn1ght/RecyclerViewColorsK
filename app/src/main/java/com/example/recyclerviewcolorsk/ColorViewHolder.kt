b4e97030ffac307ecd2e68090725d5eca72c7df4
N1ckn1ght <n1ckn1ght@protonmail.com>
2022-02-06 20:55:22 +0800

    help

---
+++ app/src/main/java/com/example/recyclerviewcolorsk/ColorViewHolder.kt
@@ -0,0 +1,21 @@
+package com.example.recyclerviewcolorsk
+
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class ColorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val tv = itemView.findViewById<TextView>(R.id.color)
+
+    init {
+        itemView.setOnClickListener{
+            Toast.makeText(itemView.context, tv.text, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun bindTo(color: Int) {
+        tv.setBackgroundColor(color)
+        tv.text = itemView.context.getString(R.string.template, color)
+    }
+}
--- app/src/main/java/com/example/recyclerviewcolorsk/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewcolorsk/MainActivity.kt
@@ -1,25 +1,25 @@
 package com.example.recyclerviewcolorsk
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.ListView
-import java.util.Random
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    val list = mutableListOf<String>("Mars", "Venus", "Earth")
+    // TODO: реализовать генерацию цветов определённой палитры
+    // val colorsList = resources.getIntArray(R.array.colors).toMutableList()
+    val colorsList = mutableListOf(Color.YELLOW, Color.RED, Color.GREEN, Color.MAGENTA)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val lv = findViewById<ListView>(R.id.list)
-        val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, list)
-        lv.adapter = adapter
-        // new code goes here:
-        val button = findViewById(R.id.roll) as Button
-        button.setOnClickListener {
-            list.shuffle()
-            adapter.notifyDataSetChanged()
-        }
+
+        val rv = findViewById<RecyclerView>(R.id.rview)
+        val colorAdapter = ColorAdapter(LayoutInflater.from(this))
+        colorAdapter.submitList(colorsList)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = colorAdapter
     }
 }
