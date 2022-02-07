dc4a567ccd41747c8602c01f4653ba8b21685ca1
N1ckn1ght <n1ckn1ght@protonmail.com>
2022-02-07 17:34:42 +0800

    fix colors.xml to colorsListArray

--- app/src/main/java/com/example/recyclerviewcolorsk/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewcolorsk/MainActivity.kt
@@ -3,22 +3,20 @@ package com.example.recyclerviewcolorsk
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    // TODO: реализовать генерацию цветов определённой палитры
-    // val colorsList = resources.getIntArray(R.array.colors).toMutableList()
-    val colorsList = mutableListOf(Color.YELLOW, Color.RED, Color.GREEN, Color.MAGENTA)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val colorsListArray = resources.getIntArray(R.array.colors).toMutableList()
         val rv = findViewById<RecyclerView>(R.id.rview)
         val colorAdapter = ColorAdapter(LayoutInflater.from(this))
-        colorAdapter.submitList(colorsList)
+        colorAdapter.submitList(colorsListArray)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = colorAdapter
     }
